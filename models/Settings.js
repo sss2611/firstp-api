489d898a21926904c02e82a631c13744a86c8165
sss2611 <solss2611@gmail.com>
2025-07-27 22:09:04 -0300

    Settings cargados

---
+++ models/Settings.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const settingSchema = new mongoose.Schema({
+  theme: { type: String },
+  marca: { type: String },
+  logo: { type: String } // Imagen como base64
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("Setting", settingSchema);
