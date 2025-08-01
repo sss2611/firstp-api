029aa84edebabb030b9c857b070cffe66f369941
sss2611 <solss2611@gmail.com>
2025-08-01 16:19:06 -0300

    funcio startServer

--- server.js
+++ server.js
@@ -15,14 +15,34 @@ app.use("/api/images", imageRoute);
 app.use('/api/config', settingRoute);
 app.use('/api/products', productRoute);
 
-// MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ Conectado a MongoDB"))
-  .catch((err) => console.error("❌ Error de conexión:", err));
+// mongoose
+//   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("✅ Conectado a MongoDB"))
+//   .catch((err) => console.error("❌ Error de conexión:", err));
+
+// const PORT = process.env.PORT || 8080;
+// try {
+//   app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`🚀 Servidor escuchando en puerto ${PORT}`);
+//   });
+// } catch (error) {
+//   console.error('❌ Error al iniciar el servidor:', error);
+// }
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Servidor escuchando en puerto ${PORT}`);
-});
+
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("✅ Conectado a MongoDB");
+
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`🚀 Servidor escuchando en puerto ${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Error al iniciar la app:", err);
+  }
+}
+
+startServer();
 
