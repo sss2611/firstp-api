61010135cf8d79359472b3899ca369383e9a688e
sss2611 <solss2611@gmail.com>
2025-08-01 23:10:23 -0300

    modifique el await de startServer

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 async function startServer() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ Conectado a MongoDB");
 
     app.listen(PORT, '0.0.0.0', () => {
